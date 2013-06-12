d7746870fd2ac4f5b01637ba19ff1fc2e797699b
Martin Baulig <martin.baulig@xamarin.com>
2013-06-13 00:04:35 +0200

    Add iOS app.

---
+++ samples/SimpleTouch/SimpleTouch/SimpleTouchViewController.cs
@@ -0,0 +1,94 @@
+//
+// SimpleTouch45ViewController.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2013 Xamarin Inc. (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Drawing;
+using System.Collections.Specialized;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Portable;
+
+namespace SimpleTouch
+{
+	public partial class SimpleTouchViewController : UIViewController
+	{
+		Power power;
+		int current;
+
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public SimpleTouchViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "SimpleTouchViewController_iPhone" : "SimpleTouchViewController_iPad", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			power = new Power (4);
+		}
+
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+
+		partial void Action (MonoTouch.Foundation.NSObject sender)
+		{
+			RunTests ();
+
+			var number = power.Compute (++current);
+			Label.Text = string.Format ("{0} is the power!", number);
+		}
+
+		static void RunTests ()
+		{
+			TestWeb.TestType ();
+
+			var windows = new TestSystemWindows ();
+			var notification = windows.Test () as INotifyCollectionChanged;
+			Console.WriteLine (notification);
+		}
+	}
+}
+
